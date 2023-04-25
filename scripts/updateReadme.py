d001f572a93308c0299205f72e864bfaf746d38b
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2023-04-25 18:25:25 +0900

    update script

--- scripts/updateReadme.py
+++ scripts/updateReadme.py
@@ -1,11 +1,7 @@
-import textwrap
-
 GENERATE_FILE_NAME = 'README.md'
 
 # dedent: 各行の先頭の空白削除
-docs_str = textwrap.dedent('''\
-![](./profile-3d-contrib/profile-south-season.svg)
-''').format(conpassList=resultList).strip()
+docs_str = '![](./profile-3d-contrib/profile-south-season.svg)'
 
 # READMEに書き込む
 with open(GENERATE_FILE_NAME, 'w') as f:
