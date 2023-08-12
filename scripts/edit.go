c599766b68a1dd03c85da90e980816343981fefd
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-08-13 00:37:44 +0900

    update profile readme

--- scripts/edit.go
+++ scripts/edit.go
@@ -6,7 +6,12 @@ import (
 
 const readmeFile = "README.md"
 const head = `### Hi 😃
-My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.`
+My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
+### Skills
+
+[<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
+[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)`
+
 const docs_str = "![](./profile-3d-contrib/profile-gitblock.svg)"
 
 func Edit() error {
