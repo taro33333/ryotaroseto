a365e8d24465604c1035cf88204a3b87cb087a67
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-08-14 17:45:43 +0900

    add kubernetes icon

--- scripts/edit.go
+++ scripts/edit.go
@@ -10,7 +10,8 @@ My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
 ### Skills
 
 [<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
-[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)`
+[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
+[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)`
 
 const docs_str = "![](./profile-3d-contrib/profile-gitblock.svg)"
 
