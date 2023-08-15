7e815be441474a25f0bec28e8df1af246ad49124
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-08-15 15:19:27 +0900

    Update readme

--- scripts/edit.go
+++ scripts/edit.go
@@ -11,7 +11,9 @@ My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
 
 [<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
 [<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
-[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)`
+[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)
+[<img src="./public/images/laravel.svg" width="40" height="40" />](https://laravel.com/)
+[<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)`
 
 const docs_str = "![](./profile-3d-contrib/profile-gitblock.svg)"
 
