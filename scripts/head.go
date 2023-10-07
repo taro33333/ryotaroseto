9bc76c6ca9ca75a870d8885f781719448f42c19c
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-10-08 01:04:19 +0900

    fix space

--- scripts/head.go
+++ scripts/head.go
@@ -6,7 +6,7 @@ func header() string {
 
 func head() string {
 	return `### Hi 😃
-	My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
+My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
 	`
 }
 
@@ -14,11 +14,11 @@ func skillsHead() string {
 	return `
 	### Skills
 
-	[<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
-	[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
-	[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)
-	[<img src="./public/images/php.svg" width="40" height="40" />](https://php.org/)
-	[<img src="./public/images/python.svg" width="40" height="40" />](https://www.python.org/)
-	[<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)
+[<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
+[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
+[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)
+[<img src="./public/images/php.svg" width="40" height="40" />](https://php.org/)
+[<img src="./public/images/python.svg" width="40" height="40" />](https://www.python.org/)
+[<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)
 	`
 }
