79b89d191996b30e16710a316e70501c10266f10
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2023-10-08 01:09:00 +0900

    fix skill header (#2)
    * fix skill header
    
    * update by github action
    
    * fix space
    
    * update by github action
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- scripts/head.go
+++ scripts/head.go
@@ -1,7 +1,18 @@
-### Hi 😃
+package scripts
+
+func header() string {
+	return head() + skillsHead()
+}
+
+func head() string {
+	return `### Hi 😃
 My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
-	
-### Skills
+	`
+}
+
+func skillsHead() string {
+	return `
+	### Skills
 
 [<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
 [<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
@@ -9,4 +20,5 @@ My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
 [<img src="./public/images/php.svg" width="40" height="40" />](https://php.org/)
 [<img src="./public/images/python.svg" width="40" height="40" />](https://www.python.org/)
 [<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)
-![](./profile-3d-contrib/profile-gitblock.svg)
+	`
+}
