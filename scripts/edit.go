43ac7704f42c2f82d61ef25edaa856bfc02dcea1
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-10-08 00:57:47 +0900

    fix skill header

--- scripts/edit.go
+++ scripts/edit.go
@@ -5,16 +5,6 @@ import (
 )
 
 const readmeFile = "README.md"
-const head = `### Hi 😃
-My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
-### Skills
-
-[<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
-[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
-[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)
-[<img src="./public/images/php.svg" width="40" height="40" />](https://php.org/)
-[<img src="./public/images/python.svg" width="40" height="40" />](https://www.python.org/)
-[<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)`
 
 const docs_str = "![](./profile-3d-contrib/profile-gitblock.svg)"
 
@@ -26,7 +16,7 @@ func Edit() error {
 	defer func() {
 		f.Close()
 	}()
-	_, err = f.Write([]byte(head + docs_str))
+	_, err = f.Write([]byte(header() + docs_str))
 	if err != nil {
 		return err
 	}
---
+++ scripts/head.go
@@ -0,0 +1,24 @@
+package scripts
+
+func header() string {
+	return head() + skillsHead()
+}
+
+func head() string {
+	return `### Hi 😃
+	My name is Ryotaro Seto. I’m Japanese. I was born and raised in Japan.
+	`
+}
+
+func skillsHead() string {
+	return `
+	### Skills
+
+	[<img src="./public/images/go.svg" width="40" height="40" />](https://golang.org/)
+	[<img src="./public/images/github.svg" width="40" height="40" />](https://github.com/)
+	[<img src="./public/images/kubernetes.svg" width="40" height="40" />](https://cloud.google.com/learn/what-is-kubernetes?hl=ja)
+	[<img src="./public/images/php.svg" width="40" height="40" />](https://php.org/)
+	[<img src="./public/images/python.svg" width="40" height="40" />](https://www.python.org/)
+	[<img src="./public/images/typescript.svg" width="40" height="40" />](https://www.typescriptlang.org/)
+	`
+}
